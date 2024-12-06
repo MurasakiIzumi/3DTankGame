6e2208854e049f84dc84a316e2e5eafe9ee07a34
MurasakiIzumi <dova879537478@gmail.com>
2024-12-06 10:24:58 +0900

    PanzerMove

---
+++ Assets/Script/PanzerMoveControl.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanzerMoveControl : MonoBehaviour
+{
+    [SerializeField] float forwardSpeed;
+    private float backSpeed;
+    [SerializeField] float rotateSpeed;
+
+    private Vector3 velocity;
+
+    void Start()
+    {
+        backSpeed = forwardSpeed / 2;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        PanzerMove();
+    }
+
+    private void PanzerMove()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertiacl = Input.GetAxis("Vertical");
+
+        velocity = new Vector3(0, 0, vertiacl);
+
+        velocity = transform.TransformDirection(velocity);
+
+        if (vertiacl > 0.1)
+        {
+            velocity *= forwardSpeed;
+        }
+        else if ((vertiacl < -0.1)) 
+        {
+            velocity *= backSpeed;
+        }
+
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+        transform.Rotate(0, horizontal * rotateSpeed, 0);
+    }
+}
