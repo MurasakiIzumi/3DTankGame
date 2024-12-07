7af95bc02545bb312825e109f7341cc265755199
MurasakiIzumi <dova879537478@gmail.com>
2024-12-07 22:54:51 +0900

    Panzer今の姿勢を示すUI

---
+++ Assets/Script/StateCameraControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateCameraControl : MonoBehaviour
+{
+    private GameObject Target;
+
+    void Start()
+    {
+        Target = GameObject.FindWithTag("StateCenter");
+    }
+
+    void FixedUpdate()
+    {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 targetPos = new Vector3(Target.transform.position.x, transform.position.y, Target.transform.position.z);
+        transform.position = targetPos;
+
+        Vector3.
+    }
+}
